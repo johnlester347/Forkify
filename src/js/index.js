c79ebc287dc6a64177455af3a5afa9b12e01896a
JohnLester <jonhlesterbalucas.pamplona@gmail.com>
2021-06-02 19:45:29 +0700

    New add code in folder dist and src

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
-import num from './test';
+import num from './test'; // Now pwede mo na magamit yung ininclude mong number sa test.js 
 const x = 29;
 console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path'); // Eto yung path 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Eto yung plugin na nag iinject ng HTML na nasa folder na src to folder dist
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/index.js'],
+    entry: ['babel-polyfill', './src/js/index.js'], // Eto yung location ng file na i mimix or i cocompile para maging isang file, 
+    //which is bundle.js
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
+        path: path.resolve(__dirname, 'dist'), // Eto yung path kung saan mo ilalagay yung compiled na js 
+        filename: 'js/bundle.js' // Eto yung name na ma cocompiled once na irun mo na yung code sa cli
     },
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist' // Eto yung dev server na mag aautomatic mag load yung page once na mag save ka ng code
+        // Eto yung folder na mag seserve sa file for production kasi yung src is for development lang
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
+            filename: 'index.html', // Eto yung name ng file na maiinject sa dist folder 
+            template: './src/index.html' // Eto yung index.html na ma cocopy or ma iinjext sa dist folder
         })
     ],
     devtool: "eval-cheap-source-map",
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
+                test: /\.js$/, // This will look for all the files that ends with .js
+                exclude: /node_modules/, // We need to use this para hindi ma apply sa node modules yung babel loader
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'babel-loader' // This is the package that convert ES6 to ES5
                 }
             }
         ]
