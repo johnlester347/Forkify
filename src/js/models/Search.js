64aeafc589aae7323aef82bb57a9f79e0a1487c6
JohnLester <jonhlesterbalucas.pamplona@gmail.com>
2021-06-03 17:08:32 +0700

    Getting result from API and display in the UI

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,9 +7,9 @@ export default class Search {
 
     async getResults(query) {
         try {
-            const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
-            this.result = res.data.recipes;
-            console.log(this.result);
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`); // Masasave lahat ng obj property sa res
+            this.result = res.data.recipes; // Ma aaccess mo lahat ng property ng res galing sa API
+            // console.log(this.result);
         } catch (error) {
             alert(error);
         }
