fd47ece2ea66a9f1b5046502dcac142f22293a7e
JohnLester <jonhlesterbalucas.pamplona@gmail.com>
2021-06-05 18:58:05 +0700

    Convert ingredients to lowercase and replacing the strings

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -29,4 +29,68 @@ export default class Recipe {
     calcServings() {
         this.serving = 4;
     }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+
+        const newIngredients = this.ingredients.map(el => { // Looping all the ingredients
+            // 1. Uniform units
+            let ingredient = el.toLowerCase(); // All the ingredients is now lower case
+            // console.log(ingredient);
+
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+
+            // 2. Remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+            // 3. Parse ingredients into count, unit and ingredient
+            const arrIng = ingredient.split(' '); // Splitting all the ingredients into an array 
+            // console.log(arrIng);
+
+            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2)); // This will going to return -1 kapag yung item ay wala
+            // sa array, then return 1 kapag yung item ay nasa array
+            // console.log(unitIndex);
+
+            let objIng;
+            if (unitIndex > -1) {
+                // There is a count
+                // Ex. 4 1/2 cups, arrCount is [4, 1/2]
+                // Ex. 4 cups, arrCount is [4]
+                const arrCount = arrIng.slice(0, unitIndex);
+
+                let count;
+                if (arrCount.length === 1) {
+                    count = eval(arrIng[0].replace('-', '+')); // replace - with a + 
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+')); // --> '4+1/2' --> eva;('4+1/2') --> 4.5
+                }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
+                };
+            } else if (parseInt(arrIng[0], 10)) {
+                // There is NO unit, but 1st element is number
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                }
+            } else if (unitIndex === -1) {
+                // There is NO unit and NO number is 1st position
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                }
+            }
+
+            return objIng;
+        });
+        this.ingredients = newIngredients;
+    }
 }
