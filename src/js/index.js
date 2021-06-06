2f1db7024f1104b9b3c189eeb60143404833fd52
JohnLester <jonhlesterbalucas.pamplona@gmail.com>
2021-06-07 00:06:12 +0700

    Adding local storage API done

--- src/js/index.js
+++ src/js/index.js
@@ -149,12 +149,6 @@ elements.shopping.addEventListener('click', e => {
 });
 
 
-// TESTING 
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
-
-
 /** LIKE CONTROLLER ***/
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
@@ -185,6 +179,21 @@ const controlLike = () => {
     // likesView.toggleLikeBtn(state.likes.getNumLikes());
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+
+// Restore liked recipes on page load 
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 
 // Handling recipe button clicks 
